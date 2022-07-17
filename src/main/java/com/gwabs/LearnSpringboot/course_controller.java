62ba280abf85f29665519bd1d12b0bd2fc7c42c0
Abubakar Abdullahi Gwabare <gwabstech@gmail.com>
2022-07-17 22:58:21 +0100

    Done

--- src/main/java/com/gwabs/LearnSpringboot/course_controller.java
+++ src/main/java/com/gwabs/LearnSpringboot/course_controller.java
@@ -1,5 +1,6 @@
 package com.gwabs.LearnSpringboot;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class course_controller {
     @GetMapping("/courses")
     public ArrayList<courses> getAllCourses(){
         return course_logic.addToList();
+    }
+
+    @GetMapping("/courses/random")
+    public courses getRandomCourse(){
+        return course_logic.getSingleCourse();
     }
 
 }
--- src/main/java/com/gwabs/LearnSpringboot/course_logic.java
+++ src/main/java/com/gwabs/LearnSpringboot/course_logic.java
@@ -1,18 +1,32 @@
 package com.gwabs.LearnSpringboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 public class course_logic {
 
 
     private ArrayList<courses> courses = new ArrayList<>();
-    courses courses1 = new courses("Intro to HTML","Abubakar",2);
+
+
 
     public ArrayList<courses> addToList(){
+        courses courses1 = new courses("Intro to HTML, CSS and JAVA","Abubakar Abdullahi Gwabare",7);
         for (int i = 0 ; i< 10 ; i++){
 
             courses.add(courses1);
         }
         return courses;
+    }
+
+    public courses getSingleCourse(){
+        addToList();
+        int min = 0;
+        Random r = new Random();
+        int index = r.nextInt(min,courses.size()-1);
+        return courses.get(index);
     }
 }
